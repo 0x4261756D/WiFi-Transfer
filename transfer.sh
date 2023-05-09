3818af7473609b2f1c1dc712240b83cd46f138ba
0x4261756D <38735823+0x4261756D@users.noreply.github.com>
2023-05-09 20:58:41 +0200

    Fix error if zip is not installed

--- transfer.sh
+++ transfer.sh
@@ -6,7 +6,10 @@ done
 cd tmp
 ../shas.sh .
 name=$(echo $@ | sed -e 's/ *//' | sha256sum | cut -c1-15).zip
-zip ./$name -r ./
+if [[ -f $(which zip) ]]
+then
+	zip ./$name -r ./
+fi
 sha256sum $name >> sha256sums.txt
 trap "cd ..; rm -rf ./tmp" SIGINT
 python3 -m http.server 7042

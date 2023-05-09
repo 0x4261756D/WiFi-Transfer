86dc7c1e49cc9f1f6aab750670613e9616a45f8a
0x4261756D <38735823+0x4261756D@users.noreply.github.com>
2023-05-09 22:46:18 +0200

    Fix zip test + recursive copying

--- transfer.sh
+++ transfer.sh
@@ -1,15 +1,22 @@
-mkdir tmp
+mkdir -p tmp
+echo "Starting to copy"
 for i in $*
 do
-	cp $i ./tmp -a
+	if [[ "$i" -ef "./" ]]
+	then
+		echo "Skipping $i"
+	else
+		cp $i ./tmp -a
+	fi
 done
+echo "Done copying"
 cd tmp
 ../shas.sh .
-name=$(echo $@ | sed -e 's/ *//' | sha256sum | cut -c1-15).zip
-if [[ -f $(which zip) ]]
+if [[ $(which zip) ]]
 then
+	name=$(echo $@ | sed -e 's/ *//' | sha256sum | cut -c1-15).zip
 	zip ./$name -r ./
+	sha256sum $name >> sha256sums.txt
 fi
-sha256sum $name >> sha256sums.txt
 trap "cd ..; rm -rf ./tmp" SIGINT
 python3 -m http.server 7042
